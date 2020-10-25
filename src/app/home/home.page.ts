58718ce021749d6445391ba995ea6c30de2d1b36
MarieM71 <71020636+MarieM71@users.noreply.github.com>
2020-10-25 09:23:29 +0000

    creation

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,15 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
-
-}
+  constructor(private alertController: AlertController) {
+  }
+    async info() {
+      const alert = await this.alertController.create({
+        message: 'Apontar com o escaner num porta-chave, apertar no botão na esquerda do dispositivo para registrar-lo no histórico. ',
+        buttons: ['OK']
+      });
+  
+      await alert.present();
+    }
+    
+  }
